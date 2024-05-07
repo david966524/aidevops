b319d8b23dfd2148b667dbe4c316ae6f575e86f9
david966524 <david9665241926@gmail.com>
2024-05-07 15:21:46 +0700

    update

--- services/resp.py
+++ services/resp.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel, Field
 
 from typing import Any
+
+
 class Resp(BaseModel):
-    HttpCode: int | str = Field(default=None,title="HttpCode", description="HttpCode")
-    Data: Any = Field(default=None,title="Data", description="Data")
+    HttpCode: int | str = Field(default=None, title="HttpCode", description="HttpCode")
+    Data: Any = Field(default=None, title="Data", description="Data")
